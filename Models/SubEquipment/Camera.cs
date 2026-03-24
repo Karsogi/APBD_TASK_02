d8e09c822e7229687d14ba1cd953f556afb7f291
Alex Kozhar <garu1996@example.com>
2026-03-24 15:22:44 +0100

    Subclasses and Inheritance

--- Models/SubEquipment/Camera.cs
+++ Models/SubEquipment/Camera.cs
@@ -1,6 +1,14 @@
-﻿namespace APBD_TASK2.Models.SubEquipment;
-
-public class Camera
+﻿namespace APBD_TASK2.Models
 {
-    
-}
+    public class Camera : Equipment
+    {
+        private int pixels { get; set; }
+        private bool isBroken { get; set; }
+
+        public Camera(string name, int megapixels, bool hasLens)
+            : base(name)
+        {
+            this.pixels = pixels;
+            this.isBroken = isBroken;
+        }
+    }
--- Models/SubEquipment/Laptop.cs
+++ Models/SubEquipment/Laptop.cs
@@ -1,6 +1,14 @@
-﻿namespace APBD_TASK2.Models.SubEquipment;
+﻿using APBD_TASK2.Models;
 
-public class Laptop
+public class Laptop : Equipment
 {
-    
+    private string Processor { get; set; }
+    private int RAM { get; set; }
+
+    public Laptop(string name, string processor, int ram)
+        : base("Laptop")
+    {
+        Processor = processor;
+        RAM = ram;
+    }
 }
