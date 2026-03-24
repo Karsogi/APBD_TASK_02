57e1c6973fd2e013066814927bfe1d2acb07b1aa
Alex Kozhar <garu1996@example.com>
2026-03-24 08:51:02 +0100

    Add sample equipment (Laptop, Camera)

--- Program.cs
+++ Program.cs
@@ -6,10 +6,17 @@ using APBD_TASK2.Models;
 using APBD_TASK2.Services;
 
 var db = Singleton.Instance;
+
 var service = new RentalService();
 var student = new User("John", "Smith", UserType.Student);
 var employee = new User("Anna", "Nowak", UserType.Employee);
 
 service.AddUser(student);
 service.AddUser(employee);
+
+var laptop = new Equipment("Laptop");
+var camera = new Equipment("Camera");
+
+service.AddEquipment(laptop);
+service.AddEquipment(camera);
 
