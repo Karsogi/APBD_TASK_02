03391f216f70875c0fc96a9e95181ee06a94e32c
Alex Kozhar <garu1996@example.com>
2026-03-24 08:39:12 +0100

    Implement RentalService with business rules

--- Interfaces/IRentalService.cs
+++ Interfaces/IRentalService.cs
@@ -1,6 +1,15 @@
-﻿namespace APBD_TASK2.Interfaces;
+﻿using APBD_TASK2.Models;
+
+namespace APBD_TASK2.Interfaces;
 
 public interface IRentalService
 {
-    
+    void AddUser(User user);
+    void AddEquipment(Equipment equipment);
+
+    void RentEquipment(string userId, string equipmentId);
+    void ReturnEquipment(string equipmentId);
+
+    List<Equipment> GetAllEquipment();
+    List<Equipment> GetAvailableEquipment();
 }
--- Services/RentalService.cs
+++ Services/RentalService.cs
@@ -1,9 +1,72 @@
 ﻿using APBD_TASK2.Interfaces;
+using APBD_TASK2.Models;
 
 namespace APBD_TASK2.Services;
 
 public class RentalService : IRentalService
 {
-    
-    
+    private List<User> users = new();
+    private List<Equipment> equipment = new();
+    private List<Rental> rentals = new();
+
+    public void AddUser(User user)
+    {
+        users.Add(user);
+    }
+
+    public void AddEquipment(Equipment eq)
+    {
+        equipment.Add(eq);
+    }
+
+    public List<Equipment> GetAllEquipment() => equipment;
+
+    public List<Equipment> GetAvailableEquipment()
+    {
+        return equipment.Where(e => e.IsAvailable).ToList();
+    }
+
+    public void RentEquipment(string userId, string equipmentId)
+    {
+        var user = users.FirstOrDefault(u => u.Id == userId);
+        var eq = equipment.FirstOrDefault(e => e.Id == equipmentId);
+
+        if (user == null || eq == null)
+            throw new Exception("User or Equipment not found");
+
+        if (!eq.IsAvailable)
+            throw new Exception("Equipment not available");
+
+        int active = rentals.Count(r => r.User.Id == userId && !r.IsReturned);
+
+        if (active >= user.MaxActiveRentals)
+            throw new Exception("User exceeded rental limit");
+
+        rentals.Add(new Rental
+        {
+            User = user,
+            Equipment = eq,
+            RentDate = DateTime.Now,
+            DueDate = DateTime.Now.AddDays(7)
+        });
+
+        eq.IsAvailable = false;
+    }
+
+    public void ReturnEquipment(string equipmentId)
+    {
+        var rental = rentals.FirstOrDefault(r =>
+            r.Equipment.Id == equipmentId && !r.IsReturned);
+
+        if (rental == null)
+            throw new Exception("Rental not found");
+
+        rental.ReturnDate = DateTime.Now;
+        rental.Equipment.IsAvailable = true;
+
+        if (rental.ReturnDate > rental.DueDate)
+        {
+            Console.WriteLine("Late return! Penalty applied.");
+        }
+    }
 }
