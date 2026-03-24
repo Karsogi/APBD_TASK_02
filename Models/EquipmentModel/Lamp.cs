d8cbde3d0e5b3f0d162fafcc64d243db5de4760d
Alex Kozhar <garu1996@example.com>
2026-03-24 14:59:51 +0100

    Subclasses and Inheritance

---
+++ Models/EquipmentModel/Lamp.cs
@@ -0,0 +1,14 @@
+﻿namespace APBD_TASK2.Models.EquipmentModel;
+
+public class Camera : Equipment
+{
+    private int pixels { get; set; }
+    private bool working { get; set; }
+
+    public Camera(int pixels, bool working)
+        : base("Camera")
+    {
+        this.pixels = pixels;
+        this.working = working;
+    }
+}
